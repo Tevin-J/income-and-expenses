0437896b402d32c95bba4cfa570f59d01fe9bf08
Anton <example@gmail.com>
2019-09-08 20:45:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+let number=5;
+let string="Hello!"
+let boolean=true;
+console.log(number);
+console.log(string);
+console.log(boolean);
+
+let person = {
+    name: "Joe",
+    age: 29,
+    isMarried: false,
+}
+
+console.log(person.isMarried);
+
+//alert ("Hello"); 
+/*let answer=confirm ("are you here?");
+console.log (answer);*/
+let answer2=prompt("are you 18?","yes");
+console.log (typeof(answer2));
